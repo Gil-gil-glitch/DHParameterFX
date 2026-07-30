6482f96fefbe05ae437e4c2d74ed169ae5247089
Gil-gil-glitch <gilsocojp@gmail.com>
2026-07-31 02:04:36 +0900

    Added a small class to act as a tag if any of the axes were clicked

---
+++ src/main/java/com/example/dhparameterfx/GizmoType.java
@@ -0,0 +1,10 @@
+package com.example.dhparameterfx;
+
+public enum GizmoType {
+    
+    DRAG_A,     // Moving along X (Red)
+    DRAG_D,     // Moving along Z (Blue)
+    ROTATE_THETA, // Rotating around Z (Gold Hub)
+    ROTATE_ALPHA  // Twisting around X (Green)
+    
+}
